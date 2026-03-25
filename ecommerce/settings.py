b7f2a00d1f7f488a1a59a599e543ef1a600981a2
Gunjan7440 <kumarig17661@gmail.com>
2026-03-25 18:26:39 +0530

    Update settings.py
    Add Render URL to ALLOWED_HOSTS

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -72,3 +72,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+ALLOWED_HOSTS = ['ecommerce-store-lj86.onrender.com', 'localhost', '127.0.0.1']
